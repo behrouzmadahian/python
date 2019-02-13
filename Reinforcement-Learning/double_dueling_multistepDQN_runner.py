449386cc614b78b07bb811688dea28c0ade940f8
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2019-02-13 12:11:35 -0500

    Update double_dueling_multistepDQN_runner.py

--- Reinforcement-Learning/double_dueling_multistepDQN_runner.py
+++ Reinforcement-Learning/double_dueling_multistepDQN_runner.py
@@ -18,26 +18,15 @@ import tensorflow as tf
 from helper import *
 from vizdoom import *
 from double_dueling_multistep_DQN import *
-
-max_episode_length = 300
-gamma = 0.99  # discount rate for advantage estimation and reward discounting
-s_size = 7056  # Observations are greyscale frames of 84 * 84 * 1
-a_size = 3  # Agent can move Left, Right, or Fire
-load_model = False
-model_path = './9-summaryFolder/model'
-# Max number of episodes for worker_0
-# if global_step equals this, we raise exception to stop ALL threads
-MAX_STEPS = 100000
-N_SAVE_MODEL = 250
-GLOBAL_TARGET_UPDATE_FREQ = 500
-# k-step DQN
-STEPS = 30
+from config_double_dueling_multistep_DQN import DQNConfig
+dqn_config = DQNConfig()
 tf.reset_default_graph()
 
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
-if not os.path.exists(model_path + '/Target_model/'):
-    os.makedirs(model_path + '/Target_model/')
+
+if not os.path.exists(dqn_config.model_path):
+    os.makedirs(dqn_config.model_path)
+if not os.path.exists(dqn_config.model_path + '/Target_model/'):
+    os.makedirs(dqn_config.model_path + '/Target_model/')
 # Create a directory to save episode playback gifs to
 if not os.path.exists('./9-summaryFolder/frames'):
     os.makedirs('./9-summaryFolder/frames')
@@ -45,23 +34,24 @@ lock = threading.Lock()
 
 with tf.device("/cpu:0"):
     global_episodes = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
-    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
-    master_network = DQN(s_size, a_size, 'global', None, 256)  # Generate global network
-    master_target_network = DQN(s_size, a_size, 'global_target', 256)
+    master_network = DQN(dqn_config.s_size, dqn_config.a_size, 'global', None, 256)  # Generate global network
+    master_target_network = DQN(dqn_config.s_size, dqn_config.a_size, 'global_target', 256)
     # num_workers = multiprocessing.cpu_count()  # Set workers to number of available CPU threads
     num_workers = 4  # Set workers to number of available CPU threads
     workers = []
     # Create worker classes
     for i in range(num_workers):
         # Worker Agent
-        workers.append(Worker(DoomGame(), i, s_size, a_size, STEPS, optimizer, model_path, global_episodes, lock))
+        workers.append(Worker(DoomGame(), i, dqn_config.s_size,
+                              dqn_config.a_size, dqn_config.STEPS, dqn_config.optimizer,
+                              dqn_config.model_path, global_episodes, lock))
     saver = tf.train.Saver(max_to_keep=5)
 with tf.Session() as sess:
     coord = tf.train.Coordinator()   # read on it!
-    if load_model:
+    if dqn_config.load_model:
         # restores values of target network
         print('Loading Model...')
-        ckpt = tf.train.get_checkpoint_state(model_path + '/Target_model/')
+        ckpt = tf.train.get_checkpoint_state(dqn_config.model_path + '/Target_model/')
         saver.restore(sess, ckpt.model_checkpoint_path)
     else:
         sess.run(tf.global_variables_initializer())
@@ -70,9 +60,9 @@ with tf.Session() as sess:
 
     worker_threads = []
     for worker in workers:
-        worker_work = lambda: worker.work(max_episode_length, gamma, sess, coord,
-                                          saver, master_target_network,
-                                          GLOBAL_TARGET_UPDATE_FREQ, N_SAVE_MODEL, MAX_STEPS)
+        worker_work = lambda: worker.work(dqn_config.max_episode_length, dqn_config.gamma, sess, coord,
+                                          saver, master_target_network, dqn_config.GLOBAL_TARGET_UPDATE_FREQ,
+                                          dqn_config.N_SAVE_MODEL, dqn_config.MAX_STEPS)
         t = threading.Thread(target=worker_work)
         t.start()
         worker_threads.append(t)
@@ -80,4 +70,4 @@ with tf.Session() as sess:
     # after all threads finish, I will save the parameters of target network to file!!
     target_network_W = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
     saver = tf.train.Saver(target_network_W)
-    saver.save(sess, model_path + '/Target_model/' + 'target_network.ckpt')
+    saver.save(sess, dqn_config.model_path + '/Target_model/' + 'target_network.ckpt')
