e0ca6fcfa00fbce75abe78fd43baeb6375048313
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2020-02-20 10:30:13 -0500

    2

--- tensorflow2/02_functionalAPI/10_GradientTape.py
+++ tensorflow2/02_functionalAPI/10_GradientTape.py
@@ -25,6 +25,25 @@ train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
 # Training loop:
+epochs = 3
+for epoch in range(epochs):
+  print('Start of epoch: {}'.format(epoch))
+  # Iterate oveer the batches of the data set
+  for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+    # Open a GradientTape to record the operations run
+    # During forward pass, which enables auto differentiation
+    With tf.GradientTape() as tape:
+      # Run the forward pass of the layer
+      # the operations that the layer applies to its input are going to be recorded on the GradientTape
+      logits = model(x_batch_train, training=True) # run the model in training mode <drop out enabled!
+      loss_value = losss_fn(y_batch_train, logits)
+      grads = tape.gradient(loss_value, model.trainable_weights)
+      # Run one step of gradient descent by updating the value of the variables to minimize the loss
+      optimizer.apply_gradients(zip(grads, model.trainable_weights))
+      
+      if step %200 == 0:
+        print('Training loss (for one batch) at step {}: {}'.format(step, float(loss_value)))
+        print('Seen so far {} . Samples'.format((step + 1 ) * 64
 
                                 
 
