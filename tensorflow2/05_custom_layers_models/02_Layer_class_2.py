f06ba53c0e12674e9a89e30b8e9a5d3f394c53b0
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2020-02-20 14:44:40 -0500

    1

--- tensorflow2/05_custom_layers_models/02_Layer_class_2.py
+++ tensorflow2/05_custom_layers_models/02_Layer_class_2.py
@@ -1,3 +1,8 @@
-
+"""
+Best Practice:
+Deferring weight creation until the shape of the inputs is known.
+In many cases, you may not know in advance the size of your inputs, and you would like to lazily create weights when
+that value becomes known, sometimes after instantiating the layer
+"""
 import tensorflow as tf
 from tensorflow import keras
