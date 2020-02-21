3411dd902190c11122096890b5a3c5ae7f2a739b
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2020-02-21 07:52:58 -0500

    4

--- tensorflow2/05_custom_layers_models/01_Layer_class.py
+++ tensorflow2/05_custom_layers_models/01_Layer_class.py
@@ -33,8 +33,6 @@ class Linear(keras.layers.Layer):
     super(Linear, self).__init__()
     self.w = self.add_weight(shape=(input_dim, units), initializer='random_normal', trainable=True)
     self.b = self.add_weight(shape=(units,), initializer='zeros', trainable=True)
-    
-    
    
   def call(self, inputs):
    return tf.matmul(inputs, self.w) + self.b
