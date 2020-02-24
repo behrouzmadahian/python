f87fc6c893e75444fcbc42bfafeb0bc4c2218d14
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2020-02-24 11:57:12 -0500

    Update 02_encode_decoder.py

--- tensorflow2/rnn_keras/02_encode_decoder.py
+++ tensorflow2/rnn_keras/02_encode_decoder.py
@@ -29,3 +29,27 @@ In addition, a RNN layer can return its final internal state(s). The returned st
 resume the RNN execution later, or to initialize another RNN. This setting is commonly used in the 
 encoder-decoder sequence-to-sequence model, where the encoder final state is used as the initial state of the decoder.
 """
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+encoder_vocab = 1000
+decoder_vocab = 2000
+encoder_input = keras.Input(shape=(None,))
+encoder_embedded = keras.layers.Embedding(input_dim=encoder_vocab, output_dim=64)(encoder_input)
+# return states in addition to output:
+output, state_h, state_c = keras.layers.LST(64, return_state=True, name='encoder')(encoder_embedded)
+encoder_state = [state_h, state_c]
+
+decoder_input = keras.Input(shape=(None,))
+decoder_embedded = keras.layers.Embedding(input_dim=decoder_vocab, output_dim=64)(decoder_input)
+
+# Pass the 2 states to a new LSTM layer as initial state:
+decoder_output = keras.layers.LSTM(64, name='decoder')(decoder_embedded, initial_state=encoder_state)
+output= keras.layers.Dense(10)(decoder_output)
+model = keras.Model([encoder_input, decoder_input], outputs=output)
+print(model.summary())
