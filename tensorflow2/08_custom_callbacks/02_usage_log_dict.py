29549034c42d69221e9c59fb4173e55777147f99
behrouzmadahian <44870508+behrouzmadahian@users.noreply.github.com>
2020-02-26 09:59:32 -0500

    Update 02_usage_log_dict.py

--- tensorflow2/08_custom_callbacks/02_usage_log_dict.py
+++ tensorflow2/08_custom_callbacks/02_usage_log_dict.py
@@ -22,53 +22,28 @@ model.compile(optimizer=keras.optimizers.RMSprop(lr=0.1), loss='mse', metrics=['
 x_train = x_train.reshape(60000, 784).astype('float32') / 255
 x_test = x_test.reshape(10000, 784).astype('float32') / 255
 
-class MyCustomCallback(keras.callbacks.Callback):
-  
-  def on_train_batch_begin(self, batch, logs=None):
-    print('Training: batch {} begins at {}'.format(batch, datetime.datetime.now().time()))
-  
-  def on train_batch_end(self, batch, logs=None):
-    print('Training: batch {} ends at {}'.format(batch, datetime.datetime.now().time()))
-    
-  def on_test_batch_begin(self, batch, logs=None):
-    print('Evaluating: batch {} begins at {}'.format(batch, datetime.datetime.now().time()))
+class LossAndErrorPrintingCallback(keras.callbacks.Callback):
+  def on_train_batch_end(self, batch, logs=None):
+    print('For batch {}, loss is {:7.2f}.'.format(batch, logs['loss']))
   
   def on_test_batch_end(self, batch, logs=None):
-    print('Evaluating: batch {} ends at {}'.format(batch, datetime.datetime.now().time()))
-  
-  def on_predict_batch_begin(self, logs=None):
-      print('Predicting: batch {} begins at {}'.format(batch, datetime.datetime.now().time()))
-  
-  def on_predict_batch_end(self, logs=None):
-      print('Predicting: batch {} ends at {}'.format(batch, datetime.datetime.now().time()))
-  
-  # you can have the _end versions as well!
-  def on_train_begin(self, logs=None):
-      """ Called at the begining of training"""
-      print('Training Started at {}'.format(batch, datetime.datetime.now().time()))
-  
-  def on_test_begin(self, logs=None):
-      """ Called at the begining of evaluation"""
-      print('Evaluation Started:  at {}'.format(batch, datetime.datetime.now().time()))
-      
-  def on_predict_begin(self, logs=None):
-      """ Called at the begining of prediction"""
-      print('Predicting: Started at {}'.format(batch, datetime.datetime.now().time()))
-  
-  def on_epoch_begin(self, epoch, logs=None):
-      """ Called at the begining of each epoch!"""
-      print('Epoch {} Starts at {}'.format(batch, datetime.datetime.now().time()))
-  
+    print('For batch {}, loss is {:7.2f}.'.format(batch, logs['loss']))
+
   def on_epoch_end(self, epoch, logs=None):
-      """ Called at the End of each epoch!"""
-      print('Epoch {} Starts at {}'.format(batch, datetime.datetime.now().time()))
+    print('The average loss for epoch {} is {:7.2f} and mean absolute error is {:7.2f}.'.format(epoch, logs['loss'], logs['mae']))
+
+model = get_model()
+
+_ = model.fit(x_train, y_train,
+          batch_size=64,
+          steps_per_epoch=5,
+          epochs=3,
+          verbose=0,
+          callbacks=[LossAndErrorPrintingCallback()])
+  
+ 
 
     
+_ = model.evaluate(x_test, y_test, batch_size=128, verbose=0, steps=20,
+          callbacks=[LossAndErrorPrintingCallback()])
 
-
-history = model.fit(x_train, y_train, batch_size=64, epochs=1, steps_per_epoch=5, verbose=0, callbacks=[MyCustomCallback()])
-
-eval_hist = model.evaluate(x_test, y_test, batch_size=128, verbose=0, steps=5, callbacks=[MyCustomCallback()])
-
-
-                    
